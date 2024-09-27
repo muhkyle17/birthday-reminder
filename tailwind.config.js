8d68bf588d47cd5d9552ccb460c634bc0c67bf87
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-27 14:46:18 -0700

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: 'var(--background)',
+        primary: {
+          DEFAULT: '#0B0D17',
+        },
+        secondary: {
+          DEFAULT: '#D0D6F9',
+        },
+        white: '#FFFFFF',
       },
     },
   },
   plugins: [],
-};
+}
