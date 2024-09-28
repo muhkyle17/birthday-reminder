a1158c8c69140a21131eb5002c5c96e9371a97d2
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-27 22:05:48 -0700

    Make nav buttons into link buttons and add hover states

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Logo from '@/public/shared/logo.svg'
 
 const Navbar = () => {
@@ -7,25 +8,25 @@ const Navbar = () => {
       <div className='w-1/2 flex flex-row items-center justify-between'>
         <Image src={Logo} alt='Navigation Logo' width={48} height={48} className='opacity-50' />
 
-        <hr className='bg-white opacity-30 w-5/6' />
+        <hr className='bg-white opacity-30 w-5/6 mb-1' />
       </div>
 
-      <div className='w-1/2 flex flex-row items-center gap-24 h-24 backdrop-blur-3xl '>
-        <hr className='bg-white w-10 opacity-30' />
+      <div className='w-1/2 flex flex-row items-center gap-24 h-24 backdrop-blur-3xl'>
+        <hr className='bg-white w-10 opacity-30 mb-1' />
 
         <div className='flex flex-row items-center gap-10 uppercase'>
-          <p className='flex flex-row gap-2 items-baseline'>
+          <Link href='/' className='nav-button flex flex-row gap-2 items-baseline'>
             <span className='typography-preset-eight font-bold'>00</span>Home
-          </p>
-          <p className='flex flex-row gap-2 items-baseline'>
+          </Link>
+          <Link href='/' className='nav-button flex flex-row gap-2 items-baseline'>
             <span className='typography-preset-eight font-bold'>01</span>Destination
-          </p>
-          <p className='flex flex-row gap-2 items-baseline'>
-            <span className='typography-preset-eight font-bold'>00</span>Crew
-          </p>
-          <p className='flex flex-row gap-2 items-baseline'>
-            <span className='typography-preset-eight font-bold'>00</span>Technology
-          </p>
+          </Link>
+          <Link href='/' className='nav-button flex flex-row gap-2 items-baseline'>
+            <span className='typography-preset-eight font-bold'>02</span>Crew
+          </Link>
+          <Link href='/' className='nav-button flex flex-row gap-2 items-baseline'>
+            <span className='typography-preset-eight font-bold'>03</span>Technology
+          </Link>
         </div>
       </div>
     </div>
