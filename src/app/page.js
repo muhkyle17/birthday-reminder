988de0afcc92499d3ac3806ee1bbb550bb058f87
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-27 19:18:38 -0700

    Set background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,10 @@
 import Image from 'next/image'
+import Navbar from '@/app/components/Navbar'
 
 export default function Home() {
   return (
-    <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      <p className='typography-preset-one'>Earth</p>
-      <p className='typography-preset-five'>Uranus</p>
-      <p className='typography-preset-nine mt-10'>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
-        aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
-        pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc
-        tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet,
-        consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In
-        nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
-        laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis.
-        Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus
-        id, mattis vel, nisi.{' '}
-      </p>
+    <div className='bg-[url("/home/background-home-desktop.jpg")] min-h-screen bg-contain bg-center bg-no-repeat h-screen w-full p-8 pb-20 gap-16 sm:p-20'>
+      <Navbar />
     </div>
   )
 }
