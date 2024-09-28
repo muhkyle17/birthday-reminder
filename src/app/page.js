b1baae12144fdacfbc5da5c101a389d8bc96ce6d
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-27 18:45:22 -0700

    Add samples

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,22 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20'></div>
+  return (
+    <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+      <p className='typography-preset-one'>Earth</p>
+      <p className='typography-preset-five'>Uranus</p>
+      <p className='typography-preset-nine mt-10'>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
+        aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
+        pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc
+        tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet,
+        consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In
+        nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
+        laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis.
+        Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+        Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus
+        id, mattis vel, nisi.{' '}
+      </p>
+    </div>
+  )
 }
