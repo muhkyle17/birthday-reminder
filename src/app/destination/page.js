b01ae5f73773cb11922a61bd155d816f559a4d0c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-30 18:18:11 -0700

    Add data and hover mini tab component

--- src/app/destination/page.js
+++ src/app/destination/page.js
@@ -2,6 +2,85 @@ import Image from 'next/image'
 
 import Navbar from '@/components/Navbar'
 import Moon from '../../../public/destination/image-moon.png'
+import Mars from '../../../public/destination/image-mars.png'
+import Europa from '../../../public/destination/image-europa.png'
+import Titan from '../../../public/destination/image-titan.png'
+
+const tabs = [
+  {
+    id: 1,
+    label: 'Moon',
+    description:
+      'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    image: Moon,
+    avgDistance: '384,400',
+    estTravelTime: 3,
+  },
+  {
+    id: 2,
+    label: 'Mars',
+    description:
+      'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+    image: Mars,
+    avgDistance: '225 Mil.',
+    estTravelTime: 9,
+  },
+  {
+    id: 3,
+    label: 'Europa',
+    description:
+      'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+    image: Europa,
+    avgDistance: '628 Mil.',
+    estTravelTime: 3,
+  },
+  {
+    id: 4,
+    label: 'Titan',
+    description:
+      'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+    image: Titan,
+    avgDistance: '1.6 Bil.',
+    estTravelTime: 7,
+  },
+]
+
+const HoverMiniTab = () => {
+  return (
+    <div className='flex flex-row gap-28'>
+      <Image src={Moon} />
+      <div className='flex flex-col'>
+        <div className='flex flex-row gap-10 typography-preset-eight text-secondary uppercase'>
+          <div>Moon</div>
+          <div>Mars</div>
+          <div>Europa</div>
+          <div>Titan</div>
+        </div>
+
+        <h1 className='typography-preset-two uppercase mt-10'>Moon</h1>
+
+        <p className='typography-preset-nine text-secondary'>
+          See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain
+          perspective and come back refreshed. While you’re there, take in some history by visiting
+          the Luna 2 and Apollo 11 landing sites.
+        </p>
+
+        <div className='mt-10 bg-[#ffffff8a] h-[0.5px]' />
+
+        <div className='mt-12 uppercase flex flex-row gap-20'>
+          <div className='flex flex-col gap-8'>
+            <p className='typography-preset-seven text-secondary'>Avg. Distance</p>
+            <p className='typography-preset-six'>384,400 km</p>
+          </div>
+          <div className='flex flex-col gap-8'>
+            <p className='typography-preset-seven text-secondary'>Est. Travel Time</p>
+            <p className='typography-preset-six'>3 days</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Destination = () => {
   return (
@@ -15,38 +94,7 @@ const Destination = () => {
             Pick your destination
           </p>
 
-          <div className='flex flex-row gap-28'>
-            <Image src={Moon} />
-            <div className='flex flex-col'>
-              <div className='flex flex-row gap-10 typography-preset-eight text-secondary uppercase'>
-                <div>Moon</div>
-                <div>Mars</div>
-                <div>Europa</div>
-                <div>Titan</div>
-              </div>
-
-              <h1 className='typography-preset-two uppercase mt-10'>Moon</h1>
-
-              <p className='typography-preset-nine text-secondary'>
-                See our planet as you’ve never seen it before. A perfect relaxing trip away to help
-                regain perspective and come back refreshed. While you’re there, take in some history
-                by visiting the Luna 2 and Apollo 11 landing sites.
-              </p>
-
-              <div className='mt-10 bg-[#ffffff8a] h-[0.5px]' />
-
-              <div className='mt-12 uppercase flex flex-row gap-20'>
-                <div className='flex flex-col gap-8'>
-                  <p className='typography-preset-seven text-secondary'>Avg. Distance</p>
-                  <p className='typography-preset-six'>384,400 km</p>
-                </div>
-                <div className='flex flex-col gap-8'>
-                  <p className='typography-preset-seven text-secondary'>Est. Travel Time</p>
-                  <p className='typography-preset-six'>3 days</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <HoverMiniTab />
         </div>
       </div>
     </div>
