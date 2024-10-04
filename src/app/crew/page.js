e3561ce5c6a41748734a06ea361767f681fc38ee
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-03 22:16:26 -0700

    Add meet your crew page structure

--- src/app/crew/page.js
+++ src/app/crew/page.js
@@ -1,7 +1,20 @@
+import Navbar from '@/components/Navbar'
+
 const Crew = () => {
   return (
-    <div>
-      <p>Crew</p>
+    <div className='bg-[url("/crew/background-crew-mobile.jpg")] sm:bg-[url("/crew/background-crew-tablet.jpg")] lg:bg-[url("/crew/background-crew-desktop.jpg")] bg-cover bg-no-repeat w-screen h-fit min-h-screen'>
+      <div className='flex flex-col items-start pt-0 lg:pt-10 w-screen lg:h-screen'>
+        <Navbar />
+
+        <div className='container mx-auto pb-20 lg:px-20 xl:px-40 mt-10 lg:mt-14 flex flex-col items-center gap-10 lg:gap-24'>
+          <p className='uppercase typography-preset-eight sm:typography-preset-five flex flex-row gap-6 lg:self-start'>
+            <span className='font-bold text-[#ffffff5f]'>02</span>
+            Meet your crew
+          </p>
+
+          {/* <HoverMiniTab /> */}
+        </div>
+      </div>
     </div>
   )
 }
