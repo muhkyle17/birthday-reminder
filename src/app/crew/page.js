25af26356f1d9e2504066b4b40f17f247e8087c3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-03 23:13:58 -0700

    Change the hover for changing through crew

--- src/app/crew/page.js
+++ src/app/crew/page.js
@@ -12,7 +12,7 @@ import AnoushehAnsari from '../../../public/crew/image-anousheh-ansari.png'
 const tabs = [
   {
     id: 1,
-    label: 'Douglas Hurley',
+    name: 'Douglas Hurley',
     position: 'Commander',
     description:
       'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
@@ -20,7 +20,7 @@ const tabs = [
   },
   {
     id: 2,
-    label: 'Mark Shuttleworth',
+    name: 'Mark Shuttleworth',
     position: 'Mission Specialist',
     description:
       'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
@@ -28,7 +28,7 @@ const tabs = [
   },
   {
     id: 3,
-    label: 'Victor Glover',
+    name: 'Victor Glover',
     position: 'Pilot',
     description:
       'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
@@ -36,7 +36,7 @@ const tabs = [
   },
   {
     id: 4,
-    label: 'Anousheh Ansari',
+    name: 'Anousheh Ansari',
     position: 'Flight Engineer',
     description:
       'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
@@ -52,37 +52,34 @@ const HoverMiniTab = () => {
     <div className='flex flex-col items-center justify-center lg:flex-row gap-10 xl:gap-28'>
       {activeTab && (
         <>
-          <motion.div
-            key={activeTab}
-            initial={{ opacity: 0, x: -150 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ type: 'tween', duration: 1, ease: 'easeInOut' }}
-            className='w-1/2'>
-            <Image src={tabContent?.image} alt={`${tabContent.label} Image`} />
-          </motion.div>
-
-          <div className='flex flex-col items-center lg:items-start w-[70%] lg:w-1/2'>
-            <div className='flex flex-row gap-10 typography-preset-eight text-secondary uppercase'>
-              {tabs.map(tab => (
-                <p
-                  key={tab.id}
-                  className={`cursor-pointer pb-3 transition-all duration-700 border-b-2 ${
-                    activeTab === tab.id ? 'border-white' : 'border-transparent'
-                  }`}
-                  onMouseEnter={() => setActiveTab(tab.id)}>
-                  {tab.label}
-                </p>
-              ))}
-            </div>
-
-            <h1 className='typography-preset-two uppercase mt-10'>{tabContent.label}</h1>
+          <div className='flex flex-col items-center lg:items-start w-[70%] lg:w-1/2 bg-orange-400//'>
+            <h1 className='typography-preset-two uppercase mt-10'>{tabContent.name}</h1>
 
             <p className='typography-preset-nine py-2 text-secondary w-full text-center lg:text-left'>
               {tabContent.description}
             </p>
 
-            <div className='mt-10 bg-[#ffffff8a] h-[0.5px]' />
+            <div className='flex flex-row gap-10 typography-preset-eight text-secondary uppercase'>
+              {tabs.map(tab => (
+                <div
+                  key={tab.id}
+                  className={`cursor-pointer pb-3 transition-all duration-700 bg-white  w-3 h-3 rounded-full ${
+                    activeTab === tab.id ? 'border-white' : 'border-transparent'
+                  }`}
+                  onMouseEnter={() => setActiveTab(tab.id)}
+                />
+              ))}
+            </div>
           </div>
+
+          <motion.div
+            key={activeTab}
+            initial={{ opacity: 0, x: 0 }}
+            animate={{ opacity: 1, x: -50 }}
+            transition={{ type: 'tween', duration: 1, ease: 'easeInOut' }}
+            className='w-1/2 bg-red-400'>
+            <Image src={tabContent?.image} alt={`${tabContent.name} Image`} />
+          </motion.div>
         </>
       )}
     </div>
