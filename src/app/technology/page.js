bd657be88c96f040795bf088534e19b0fff8a809
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-04 23:30:27 -0700

    Display page label

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -13,7 +13,13 @@ const Technology = () => {
     <div className='bg-[url("/technology/background-technology-mobile.jpg")] sm:bg-[url("/technology/background-technology-tablet.jpg")] lg:bg-[url("/technology/background-technology-desktop.jpg")] bg-cover bg-no-repeat w-screen h-fit min-h-screen'>
       <div className='flex flex-col items-start pt-0 lg:pt-10 w-screen lg:h-screen'>
         <Navbar />
-        <p>Technology</p>
+
+        <div className='container mx-auto mb-20 px-10 lg:px-20 xl:px-40 mt-10 lg:mt-14 flex flex-col items-center gap-10'>
+          <p className='uppercase typography-preset-eight sm:typography-preset-five flex flex-row gap-6 md:self-start'>
+            <span className='font-bold text-[#ffffff5f]'>02</span>
+            Space Launch 101
+          </p>
+        </div>
       </div>
     </div>
   )
