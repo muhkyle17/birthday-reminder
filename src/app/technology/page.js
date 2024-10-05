3d7c947f384334f503ad49d553e2114c60ea08e5
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-04 23:36:25 -0700

    Add data tabs

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -8,6 +8,38 @@ import SpaceportLandscape from '../../../public/technology/image-spaceport-lands
 import SpaceCapsulePortrait from '../../../public/technology/image-space-capsule-portrait.jpg'
 import SpaceCapsuleLandscape from '../../../public/technology/image-space-capsule-landscape.jpg'
 
+const tabs = [
+  {
+    id: 1,
+    technology: 'Launch Vehicle',
+    description:
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    id: 2,
+    technology: 'Spaceport',
+    description:
+      'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
+  },
+  {
+    id: 3,
+    technology: 'Space Capsule',
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
+]
+
+const HoverMiniTab = () => {
+  const [activeTab, setActiveTab] = useState(1)
+  // const tabContent = tabs.find(tab => tab.id === activeTab)
+
+  return (
+    <div>
+      <p>Hover Mini Tab</p>
+    </div>
+  )
+}
+
 const Technology = () => {
   return (
     <div className='bg-[url("/technology/background-technology-mobile.jpg")] sm:bg-[url("/technology/background-technology-tablet.jpg")] lg:bg-[url("/technology/background-technology-desktop.jpg")] bg-cover bg-no-repeat w-screen h-fit min-h-screen'>
@@ -19,6 +51,8 @@ const Technology = () => {
             <span className='font-bold text-[#ffffff5f]'>02</span>
             Space Launch 101
           </p>
+
+          <HoverMiniTab />
         </div>
       </div>
     </div>
