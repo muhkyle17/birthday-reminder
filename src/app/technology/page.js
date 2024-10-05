3f7bbc4a75f12360fa38b6bfe48d8790f41a2e8a
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-04 23:05:40 -0700

    Import asssets

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -1,3 +1,13 @@
+'use client'
+
+import Navbar from '@/components/Navbar'
+import LaunchVehiclePortrait from '../../../public/image-launch-vehicle-portrait.jpg'
+import LaunchVehicleLandscape from '../../../public/image-launch-vehicle-landscape.jpg'
+import SpaceportPortrait from '../../../public/image-spaceport-portrait.jpg'
+import SpaceportLandscape from '../../../public/image-spaceport-landscape.jpg'
+import SpaceCapsulePortrait from '../../../public/image-space-capsule-portrait.jpg'
+import SpaceCapsuleLandscape from '../../../public/image-space-capsule-landscape.jpg'
+
 const Technology = () => {
   return (
     <div>
